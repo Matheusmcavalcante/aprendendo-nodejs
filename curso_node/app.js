5d949ce5d92a9d1536447648592753c68babd069
Matheusmcavalcante <matheusmcavalcante@hotmail.com>
2017-03-27 15:08:20 -0300

    Add files via upload
    arquivos

---
+++ curso_node/app.js
@@ -0,0 +1,14 @@
+var app = require('./config/server');
+
+var rotaHome = require('./app/routes/home')(app);
+
+
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
+
+var rotaNoticias = require('./app/routes/noticias')(app);
+
+
+
+app.listen(3000, function(){
+	console.log('Server iniciado');
+});
